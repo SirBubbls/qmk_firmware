c913a897bff48e0a574e4e0952f7a90749f664c5
SirBubbls <lucassas@live.de>
2020-03-10 16:31:40 +0100

    backslash tap dance action

--- keyboards/fc980c/keymaps/sirbubbls-colemak/keymap.c
+++ keyboards/fc980c/keymaps/sirbubbls-colemak/keymap.c
@@ -16,6 +16,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
+enum {
+    BACKSLASH = 0,
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [BACKSLASH] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_BSLS), // replace with your keyco.des
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT( // default
         KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS, KC_PGUP, KC_PGDN,
